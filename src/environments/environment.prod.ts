38d7208a0f0322cae2860a512ed2e04edbeb6949
Bavind <100546278+kbavind@users.noreply.github.com>
2023-11-20 19:59:31 +0800

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyB_yCMv1I1O5f63gBY2LfxNhJxIfJniD08",
+    authDomain: "heal-app-eefdf.firebaseapp.com",
+    projectId: "heal-app-eefdf",
+    storageBucket: "heal-app-eefdf.appspot.com",
+    messagingSenderId: "129403093489",
+    appId: "1:129403093489:web:7e60b4a8dc2889cbb34450",
+    measurementId: "G-8ZNTRYCC18"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API_URL: 'https://newsapi.org/v2/',
+  API_Key: '384d1e813f0343be8a73effbf1ff60fd',
+  firebaseConfig:{
+    apiKey: "AIzaSyB_yCMv1I1O5f63gBY2LfxNhJxIfJniD08",
+    authDomain: "heal-app-eefdf.firebaseapp.com",
+    projectId: "heal-app-eefdf",
+    storageBucket: "heal-app-eefdf.appspot.com",
+    messagingSenderId: "129403093489",
+    appId: "1:129403093489:web:7e60b4a8dc2889cbb34450",
+    measurementId: "G-8ZNTRYCC18"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
